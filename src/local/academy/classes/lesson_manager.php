6507f6e989e223d94629e5152e77bbe553144188
Abdulrahim <abdulrahimsakr01@gmail.com>
2026-07-06 15:21:47 +0300

    fix bug at notification

--- src/local/academy/classes/lesson_manager.php
+++ src/local/academy/classes/lesson_manager.php
@@ -573,15 +573,16 @@ class lesson_manager {
         $reminder_minutes = settings_manager::get('lesson_start_reminder_minutes');
         if ($reminder_minutes > 0) {
             $run_time = (int)$time - ($reminder_minutes * 60);
-            if ($run_time > time()) {
-                $task = new \local_academy\task\lesson_start_reminder_task();
-                $task->set_next_run_time($run_time);
-                $task->set_custom_data(array(
-                    'lessonid'       => $lesson->id,
-                    'confirmed_time' => (int)$time,
-                ));
-                \core\task\manager::queue_adhoc_task($task);
+            if ($run_time < time()) {
+                $run_time = time(); // Queue it to run immediately
             }
+            $task = new \local_academy\task\lesson_start_reminder_task();
+            $task->set_next_run_time($run_time);
+            $task->set_custom_data(array(
+                'lessonid'       => $lesson->id,
+                'confirmed_time' => (int)$time,
+            ));
+            \core\task\manager::queue_adhoc_task($task);
         }
         
         $transaction->allow_commit();
