ca7a3ecf8ad914f91c53f5d9eb3e6bfc58baa4a8
Abdulrahim <abdulrahimsakr01@gmail.com>
2026-07-28 15:01:53 +0300

    add NODE_ENV var

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  basePath: "/nextjs-frontend-student",
-  assetPrefix: "/nextjs-frontend-student",
+  ...(isProd && {
+    basePath: "/nextjs-frontend-student",
+    assetPrefix: "/nextjs-frontend-student",
+  }),
 
   images: {
     remotePatterns: [
